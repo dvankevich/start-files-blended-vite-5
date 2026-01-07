4935b45420c847f395e997626e74ebdf686b2150
dvankevich <dvankevich@gmail.com>
2026-01-07 16:50:25 +0200

    add routes. use navigator.geolocation.getCurrentPosition

--- src/service/opencagedataApi.js
+++ src/service/opencagedataApi.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const getUserInfo = async ({ latitude, longitude }) => {
   const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
-  const apiKey2 = 'd4683b09d0c94ec0aebf0b2e043decbf';
   const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}`;
   const { data } = await axios.get(urlPosition, {
     params: {
